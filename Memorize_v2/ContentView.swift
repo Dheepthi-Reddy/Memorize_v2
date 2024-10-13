ea2ee326591f404fd0e23912e9800bbb07dfb9a9
Dheepthi-Reddy <dheepthireddyv@gmail.com>
2024-10-13 14:51:26 -0500

    Initial Commit

---
+++ Memorize_v2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Memorize_v2
+//
+//  Created by Dheepthi Reddy Vangeti on 10/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Memorize_v2/Memorize_v2App.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize_v2App.swift
+//  Memorize_v2
+//
+//  Created by Dheepthi Reddy Vangeti on 10/13/24.
+//
+
+import SwiftUI
+
+@main
+struct Memorize_v2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
